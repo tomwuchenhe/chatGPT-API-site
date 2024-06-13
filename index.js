252a0d27ff8694be35a42dda0d0409c0cf0ebfe0
chenhe wu <chenhewu@chenhes-MBP.gateway.sasktel.net>
2024-06-13 14:35:13 -0600

    push by chenhe

---
+++ index.js
@@ -0,0 +1,55 @@
+import express from "express"
+import axios from "axios"
+import ejs from "ejs"
+
+const app = express()
+const port = 3000
+const api_key = "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"
+const base_url = "https://api.openai.com/v1/chat/completions"
+const config = {
+    headers: {
+        Authorization: `Bearer ${api_key}`
+    }
+}
+
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+
+let conversationHistory = [];
+
+app.get("/", (req, res) => {
+    res.render("index.ejs");
+});
+
+app.post("/submit", async (req, res) => {
+    const user_content = req.body["user-in"];
+    conversationHistory.push({ role: "user", content: user_content });
+
+    try {
+        const body = {
+            "model": "gpt-3.5-turbo",
+            "messages": conversationHistory
+        };
+        const response = await axios.post(base_url, body, config);
+        const botResponse = response.data.choices[0].message.content;
+        conversationHistory.push({ role: "assistant", content: botResponse });
+        console.log(conversationHistory);
+        res.render("index.ejs", { data: conversationHistory });
+    } catch (error) {
+        console.log(error);
+        console.log("something went wrong");
+    }
+});
+
+app.listen(port, () => {
+    console.log(`listening at http://localhost:${port}`);
+});
+
+
+/*try{
+    const response = await axios.get(base_url, config)
+    console.log(response.data)
+    res.send("welcome to the index page")
+} catch (error){
+    console.log("something is wrong")
+}*/
